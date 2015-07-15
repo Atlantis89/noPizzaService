79c3d1c75dc5bea1c428bb5f8665cd33a494fb37
floatec <mp@floatec.de>
2015-07-15 14:57:28 +0200

    targets and walk

--- js/states/preloader.js
+++ js/states/preloader.js
@@ -1,20 +1,22 @@
-var preloader = function(game){
+var preloader = function(game) {
 
 };
 
 
 preloader.prototype = {
-    preload:function(){
-        this.game.load.image('rot', 'assets/rot.png');
-        this.game.load.image('gruen', 'assets/gruen.png');
-        this.game.load.image('blau', 'assets/blau.png');
-          this.game.load.image('mapTilesTest', 'assets/mapTilesTest.png');
+  preload: function() {
+    this.game.load.image('rot', 'assets/rot.png');
+    this.game.load.image('gruen', 'assets/gruen.png');
+    this.game.load.image('blau', 'assets/blau.png');
+    this.game.load.image('player', 'assets/player.png');
+    this.game.load.image('target', 'assets/target.png');
+    this.game.load.image('mapTilesTest', 'assets/mapTilesTest.png');
+    this.game.load.tilemap('map', 'assets/map.json', null, Phaser.Tilemap.TILED_JSON);
 
-          this.game.load.tilemap('map', 'assets/map.json', null, Phaser.Tilemap.TILED_JSON);
-
-        key = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-    },
-    create: function(){
-        this.game.state.start('Rot');
-    }
+    key = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+  },
+  create: function() {
+    this.game.state.start('Rot');
+  }
 };
+ 
--- js/states/rot.js
+++ js/states/rot.js
@@ -1,27 +1,82 @@
-var rot = function(game){
-    console.log("bin in rot");
+var rot = function(game) {
+  console.log("bin in rot");
 };
 
 
 rot.prototype = {
-    create: function(){
-        this.background = this.game.add.sprite(0,0, 'rot');
-        this.game.physics.startSystem(Phaser.Physics.ARCADE);
+  create: function() {
+    this.SPEED=100;
+    this.game.physics.startSystem(Phaser.Physics.ARCADE);
 
-    map = this.game.add.tilemap('map');
+    this.map = this.game.add.tilemap('map');
 
-    map.addTilesetImage('mapTilesTest');
+    this.map.addTilesetImage('mapTilesTest');
 
     // map.setCollisionBetween(1, 12);
+    this.layers = {}
+    this.layers.ground = this.map.createLayer('walkable');
 
-    layer = map.createLayer('walkable');
+    this.layers.buildings = this.map.createLayer('buildings');
 
-    layer.resizeWorld();
-    },
-    update: function(){
-        console.log(this.background);
-        if(key.isDown){
-            this.game.state.start('Gruen');
-        }
+    //collision on blockedLayer
+    this.map.setCollisionBetween(1, 100 * 100, true, 'buildings');
+    this.layers.ground.resizeWorld();
+    //create player
+
+    //we know there is just one result
+    this.player = this.game.add.sprite(80, 80, 'player');
+    this.game.physics.arcade.enable(this.player);
+
+    //the camera will follow the player in the world
+    this.game.camera.follow(this.player);
+
+    //move player with cursor keys
+    this.cursors = this.game.input.keyboard.createCursorKeys();
+
+
+    targets = this.findObjectsByType('target', this.map, 'objects');
+    target = targets[Math.floor(Math.random() * targets.length)];
+    console.log(targets)
+    target.sprite = this.game.add.sprite(target.x, target.y, 'target');
+
+  },
+  update: function() {
+    this.game.physics.arcade.collide(this.player, this.layers.buildings);
+    //player movement
+    this.player.body.velocity.y = 0;
+    this.player.body.velocity.x = 0;
+
+    if(this.cursors.up.isDown) {
+      this.player.body.velocity.y -= this.SPEED;
     }
+    else if(this.cursors.down.isDown) {
+      this.player.body.velocity.y += this.SPEED;
+    }
+    if(this.cursors.left.isDown) {
+      this.player.body.velocity.x -= this.SPEED;
+    }
+    else if(this.cursors.right.isDown) {
+      this.player.body.velocity.x += this.SPEED;
+    }
+    if (key.isDown) {
+      this.game.state.start('Gruen');
+    }
+  },
+
+  //find objects in a Tiled layer that containt a property called "type" equal to a certain value
+  findObjectsByType: function(type, map, layer) {
+    console.log(map)
+    var result = new Array();
+    map.objects[layer].forEach(function(element){
+      console.log(element)
+      if(element.type === type) {
+        //Phaser uses top left, Tiled bottom left so we have to adjust the y position
+        //also keep in mind that the cup images are a bit smaller than the tile which is 16x16
+        //so they might not be placed in the exact pixel position as in Tiled
+        element.y -= map.tileHeight;
+        result.push(element);
+      }
+    });
+    return result;
+  }
 }
